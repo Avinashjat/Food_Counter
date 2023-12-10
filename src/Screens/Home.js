b6cf65a7bd530a671ca948a4790cd898657d0f42
Avinash Jat <dev.avinashjat@gmail.com>
2023-12-11 05:08:41 +0530

    fetch the foodcategory and food item data from mongodb and show on the fronted

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{ useState, useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import Cards from '../components/Cards'
@@ -6,15 +6,67 @@ import Carsusel from '../components/Carsusel'
 
 
 export default function Home() {
+
+  const [foodItem, setfoodItem] = useState([]);
+  const [foodCategory, setfoodCategory] = useState([]);
+
+const loadData = async ()=>{
+  let response = await fetch("http://localhost:5000/api/FoodData",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    }
+  });
+  response = await response.json();
+  setfoodItem(response[0]);
+  setfoodCategory(response[1]);
+
+  // console.log(response[0],response[1]);
+}
+
+
+useEffect(()=>{
+  loadData();  
+},[])
+
+
   return (
     <>
       <div> <NavBar  /> </div>
        <div> <Carsusel /> </div>
-      <div className='m-3'> <Cards />
+      <div className='container'>   
+      
+      {
+  foodCategory && foodCategory.length !== 0 ? (
+    foodCategory.map((data) => (
+      <div key={data._id}>
+        <div className='fs-3 m-3 '>{data.CategoryName}</div>
+        <hr />
+        {
+          foodItem && foodItem.length !== 0 ? (
+            foodItem
+              .filter((item) => item.CategoryName === data.CategoryName)
+              .map((filteritem) => (
+                <div key={filteritem._id}>
+                  <Cards></Cards>
+                </div>
+              ))
+          ) : (
+            <div>No such data found</div>
+          )
+        }
+      </div>
+    ))
+  ) : (
+    <div>No food categories found</div>
+  )
+}
+
+
+
+      
       <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
+      
        </div>
 
       <div> <Footer/> </div>
